0c426790f61f5718f430c7a92c446470ec32aa2a
AlessioGalluccio <alessio.galluccio@mail.polimi.it>
2021-01-03 18:13:37 +0100

    Add descriptions in Detail page

--- Detail.js
+++ Detail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
 import BackButton from './buttons/BackButton'
 
 const titolo = 'titolo';
@@ -7,25 +7,43 @@ const descrizione = 'descrizione';
 const image_plant = require('./plants_images/banana.jpg');
 
 const Detail = () => (
-  <View style={container}>
+  <SafeAreaView style={container}>
     <BackButton/> 
     <Text style={text}>Banana</Text>
     <Image 
       style={stylesImage.stretch}
       source={image_plant}
       /> 
-    <Info 
-      title={titolo}
-      descr={descrizione}
-    />
-  </View>
+    <SafeAreaView style={{flex: 1,flexDirection: 'column'}}>
+      <Info 
+        title={'plant type'}
+        descr={'Fruit plant'}
+      />
+      <Info 
+        title={'position'}
+        descr={'Internal'}
+      />
+      <Info 
+        title={'exposition'}
+        descr={'Sunny'}
+      />
+      <Info 
+        title={'room'}
+        descr={'Living room'}
+      />
+      <Info 
+        title={'location'}
+        descr={"Av. d'Icària, 18908005 Barcelona, Spagna"}
+      />
+    </SafeAreaView>
+  </SafeAreaView>
 );
 
 const Info = (props) => (
-<View style={container}>
-    <Text style={text}>{props.title}</Text>
-    <Text style={{fontSize: 20, color: '#000'}}>{props.descr}</Text>
-  </View>
+<SafeAreaView style={container}>
+    <Text style={{fontSize: 12, color: '#000', fontWeight: 'bold'}}>{props.title}</Text>
+    <Text style={{fontSize: 18, color: '#000'}}>{props.descr}</Text>
+  </SafeAreaView>
 );
 
 
@@ -33,7 +51,7 @@ const page = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    paddingTop: 50,
+    //paddingTop: 50,
     backgroundColor: '#fff',
   },
   text: {
