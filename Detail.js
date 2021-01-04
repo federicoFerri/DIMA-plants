f9b5e7d3bbd9b53d44a32ab1b6ab18e8389fa5ce
AlessioGalluccio <alessio.galluccio@mail.polimi.it>
2021-01-04 20:01:45 +0100

    Add font Comfortaa-Regular

--- Detail.js
+++ Detail.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
 import BackButton from './buttons/BackButton'
+import { useFonts } from 'expo-font';
 
 const titolo = 'titolo';
 const descrizione = 'descrizione';
 const image_plant = require('./plants_images/banana.jpg');
 
+const DetailScreen = () => {
+  //hook to get font
+  const [loaded] = useFonts({
+    Comfortaa: require('./assets/fonts/Comfortaa-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return <SafeAreaView><Text>no</Text></SafeAreaView>;
+  }
+
+  return (
+    <Detail/>
+  );
+}
+
 const Detail = () => (
   <SafeAreaView style={container}>
     <BackButton/> 
-    <Text style={text}>Banana</Text>
-    <Image 
-      style={stylesImage.stretch}
+    <Text style={{fontSize: 36,
+    color: '#000',
+    fontFamily:'Comfortaa'}}>Banana</Text>
+    <PlantImage 
       source={image_plant}
       /> 
     <SafeAreaView style={{flex: 1,flexDirection: 'column'}}>
@@ -39,10 +56,19 @@ const Detail = () => (
   </SafeAreaView>
 );
 
+const PlantImage = (props) => (
+  <SafeAreaView style={{justifyContent: 'center', alignItems: 'center', marginHorizontal: 30,marginVertical: 10}}>
+    <Image 
+      style={stylesImage.stretch}
+      source={props.source}
+      /> 
+  </SafeAreaView>
+)
+
 const Info = (props) => (
 <SafeAreaView style={container}>
-    <Text style={{fontSize: 12, color: '#000', fontWeight: 'bold'}}>{props.title}</Text>
-    <Text style={{fontSize: 18, color: '#000'}}>{props.descr}</Text>
+    <Text style={{fontSize: 12, color: '#000', fontWeight: 'bold', fontFamily:'Comfortaa'}}>{props.title}</Text>
+    <Text style={{fontSize: 18, color: '#000', fontFamily:'Comfortaa'}}>{props.descr}</Text>
   </SafeAreaView>
 );
 
@@ -50,13 +76,18 @@ const Info = (props) => (
 const page = StyleSheet.create({
   container: {
     flex: 1,
+    
     padding: 24,
-    //paddingTop: 50,
+    paddingTop: 10,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
+    marginVertical: 15,
+    marginHorizontal: 5,
   },
   text: {
     fontSize: 30,
     color: '#000',
+    fontFamily:'Comfortaa',
   },
 });
 
@@ -80,7 +111,9 @@ const stylesImage = StyleSheet.create({
     width: 357,
     height: 151,
     flexDirection: 'row',
-    resizeMode: 'stretch',
+    resizeMode: 'contain',
+    //marginHorizontal: 10,
+    //marginVertical: 10,
   },
 });
 
@@ -89,4 +122,4 @@ const container = StyleSheet.compose(page.container, lists.listContainer);
 const text = StyleSheet.compose(page.text, lists.listItem);
 
 
-export default Detail;
+export default DetailScreen;
--- buttons/BackButton.js
+++ buttons/BackButton.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 
 const image_BackButton = require('../button_images/back_button.png');
 
 const BackButton = () => (
-  <TouchableOpacity activeOpacity={0.5}>
-      <Image 
-          style={stylesImage.backButton}
-          source={image_BackButton}
-          /> 
-  </TouchableOpacity>
+  <SafeAreaView>
+    <TouchableOpacity activeOpacity={0.5}>
+        <Image 
+            style={stylesImage.backButton}
+            source={image_BackButton}
+            /> 
+    </TouchableOpacity>
+  </SafeAreaView>
 );
 
 
@@ -19,7 +21,7 @@ const stylesImage = StyleSheet.create({
       width: 20,
       height: 21,
       resizeMode: 'contain',
-      paddingTop: 5,
+      marginTop: 20,
     },
   });
 
