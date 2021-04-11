ce047a21be1b69cab94405cccb7ccd3d46b2e9a1
AlessioGalluccio <alessio.galluccio@mail.polimi.it>
2021-04-11 15:48:27 +0200

    Add PickerPlant

--- components/CreatePlantStep2Screen.js
+++ components/CreatePlantStep2Screen.js
@@ -5,6 +5,7 @@ import ForwardButton from '../buttons/ForwardButton';
 import HeaderCreatePlant from './HeaderCreatePlant';
 import RNPickerSelect from 'react-native-picker-select';
 import DownArrow from '../buttons/DownArrow';
+import PickerPlant from './PickerPlants';
 
 class CreatePlantStep2Screen extends React.Component {
     state = {
@@ -67,50 +68,24 @@ class CreatePlantStep2Screen extends React.Component {
                 />
                 <Text style={{fontSize: 36, color: '#000', fontFamily:'Comfortaa', marginLeft: 20, marginTop: 15}}>Position</Text>
 
-                <SafeAreaView style={{ height: 52, marginLeft: 20, marginRight:20, marginTop:20,  borderColor: 'black', borderWidth: 2, fontSize: 15, color: '#000', fontFamily:'Comfortaa', padding:15}}>
-                    <RNPickerSelect
-                        style={{}}
-                        textInputProps={{fontSize: 15, color: '#000', padding: Platform.OS === 'ios' ? 13 : 0}}
-                        useNativeAndroidPickerStyle={false}
-                        placeholder={{
-                            label: 'External/Internal',
-                            value: null,
-                            color: 'gray',
-                          }}
-                        onValueChange={(value) => this.handleExternalInternal(value)}
-                        
-                        Icon={() => {
-                            return <DownArrow/>;
-                          }}
-                        items={[
-                            { label: 'External', value: 'external' },
-                            { label: 'Internal', value: 'internal' },
-                        ]}
-                    />
-                </SafeAreaView>
+                <PickerPlant
+                    label='External/Internal'
+                    items={[
+                        { label: 'External', value: 'external' },
+                        { label: 'Internal', value: 'internal' },
+                    ]}
+                    function={this.handleExternalInternal} 
+                />
 
-                <SafeAreaView style={{ height: 52, marginLeft: 20, marginRight:20, marginTop:20,  borderColor: 'black', borderWidth: 2, fontSize: 15, color: '#000', fontFamily:'Comfortaa', padding:15}}>
-                    <RNPickerSelect
-                        style={{}}
-                        textInputProps={{fontSize: 15, color: '#000', padding: Platform.OS === 'ios' ? 13 : 0}}
-                        useNativeAndroidPickerStyle={false}
-                        placeholder={{
-                            label: 'Exposition',
-                            value: null,
-                            color: 'gray',
-                          }}
-                        onValueChange={(value) => this.handleExposition(value)}
-                        
-                        Icon={() => {
-                            return <DownArrow/>;
-                          }}
-                        items={[
-                            { label: 'Full Sun', value: 'full_sun' },
-                            { label: 'Part Shade', value: 'part_shade' },
-                            { label: 'Full Shade', value: 'full_shade' },
-                        ]}
-                    />
-                </SafeAreaView>
+                <PickerPlant
+                    label='Exposition'
+                    items={[
+                        { label: 'Full Sun', value: 'full_sun' },
+                        { label: 'Part Shade', value: 'part_shade' },
+                        { label: 'Full Shade', value: 'full_shade' },
+                    ]}
+                    function={this.handleExposition} 
+                />
 
                 <TextInput
                     style={{ height: 52, marginLeft: 20, marginRight:20, marginTop:20,  borderColor: 'black', borderWidth: 2, fontSize: 15, color: '#000', padding:15}}
